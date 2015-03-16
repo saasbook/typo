7d09c7e8b77a628b6e872624020e3a71bdc8305a
Riyaz Shah <riyazshah@berkeley.edu>
2015-03-16 19:56:17 -0300

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -111,6 +111,7 @@ Rails.application.routes.draw do
     match "/admin/#{i}", :to => "admin/#{i}#index", :format => false
     match "/admin/#{i}(/:action(/:id))", :to => "admin/#{i}", :action => nil, :id => nil, :format => false
   end
+    match "/admin/content/merge", :to => "admin/content#merge", :as => "merge"
 
   # default
   root :to  => 'articles#index', :format => false
